2a56bd6947fbd45f0e0a05185c5ca8ce0e82cf0a
Torben Poguntke <torben.poguntke@worldmobile.io>
2023-09-13 13:29:01 +0200

    cp

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,8 +3,6 @@ pub type Result<T> = std::result::Result<T, cdp::provider::error::DataProviderEr
 
 #[tokio::main]
 pub async fn main() {
-    //let config = wm_ctx_service::modules::config::ConfigRoot::new(None);
-    //config.set_as_env().unwrap();
     pretty_env_logger::init();
     let config = cdp::config::ConfigRoot::new(&None).unwrap();
     config.set_as_env();
